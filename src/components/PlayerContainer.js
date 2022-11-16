43a82a6b4f74984d291988cd5d8b2ca48f79982d
alexbotha <alexbotha@hotmail.co.uk>
2022-11-16 21:11:38 +0000

    will reorganise structure of buttons

--- src/components/PlayerContainer.js
+++ src/components/PlayerContainer.js
@@ -18,21 +18,25 @@ function PlayerContainer({ players, loading, getSearchInput }) {
       <div className="database allText">
         <h1>GOAT index.</h1>
         <hr></hr>
-        <form>
-          <input
-            type="text"
-            value={search}
-            onChange={(e) => onChange(e.target.value)}
-            placeholder="Search player..."
-            autoFocus
-          />
-        </form>
+        <br></br>
+        <AddPlayerButton />
         <p>
           Below you will find an array of the greatest players to ever grace a
           Football pitch.
         </p>
+
         <br></br>
-        <AddPlayerButton />
+        <div className="inputfields">
+          <form>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => onChange(e.target.value)}
+              placeholder="Search player name..."
+              autoFocus
+            />
+          </form>
+        </div>
       </div>
       {players.map((player) => (
         <PlayerItem key={player.id} player={player}></PlayerItem>
