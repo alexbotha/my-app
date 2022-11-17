c726c5f3e4b552b309746cc70d2702458ffb0075
alexbotha <alexbotha@hotmail.co.uk>
2022-11-17 11:26:40 +0000

    created search componenet for tidy code

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,1 +1,26 @@
+import React from "react";
+import { useState } from "react";
 
+function Search({ searchInputFunction }) {
+  const [search, setSearch] = useState("");
+
+  function onChange(e) {
+    setSearch(e);
+    searchInputFunction(e);
+  }
+  return (
+    <div className="inputfields">
+      <form>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder="Search player name..."
+          autoFocus
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Search;
